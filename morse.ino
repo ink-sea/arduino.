eba876b3570cdc859256502187b16c3d09272f17
ink-sea <52481142+ink-sea@users.noreply.github.com>
2019-07-04 15:00:27 +0800

    Update morse.ino

--- morse.ino
+++ morse.ino
@@ -22,34 +22,33 @@ void loop()
   {
     switch(str[i])
     {
-      case 'a':morse.dot();morse.dash();morse.c_space();
-      case 'b':morse.dash();morse.dot();morse.dot();morse.c_space();
-      case 'c':morse.dash();morse.dot();morse.dot();morse.dot();morse.c_space();
-      case 'd':morse.dash();morse.dot();morse.dot();morse.c_space();
-      case 'e':morse.dot();morse.c_space();
-      case 'f':morse.dot(),morse.dot(),morse.dash(),morse.dot(),morse.c_space();
-      case 'g':morse.dash();morse.dash();morse.dot();morse.c_space();
-      case 'h':morse.dot();morse.dot();morse.dot();morse.dot();morse.c_space();
-      case 'i':morse.dot();morse.dot();morse.c_space();
-      case 'j':morse.dot();morse.dash();morse.dash();morse.dash();morse.c_space();
-      case 'k':morse.dash();morse.dot();morse.dash();morse.c_space();
-      case 'l':morse.dot();morse.dash();morse.dot();morse.dot();morse.c_space();
-      case 'm':morse.dash();morse.dash();morse.c_space();
-      case 'n':morse.dash();morse.dash();morse.c_space();
-      case 'o':morse.dash();morse.dash();morse.dash();morse.c_space();
-      case 'p':morse.dot();morse.dash();morse.dash();morse.dot();morse.c_space();
-      case 'q':morse.dash();morse.dash();morse.dot();morse.dash();morse.c_space();
-      case 'r':morse.dot();morse.dash();morse.dot();morse.c_space();
-      case 's':morse.dot();morse.dot();morse.dot();morse.c_space();
-      case 't':morse.dash();morse.c_space();
-      case 'u':morse.dot();morse.dot();morse.dash();morse.c_space();
-      case 'v':morse.dot();morse.dot();morse.dot();morse.dash();morse.c_space();
-      case 'w':morse.dot();morse.dash();morse.dash();morse.c_space();
-      case 'x':morse.dash();morse.dot();morse.dot();morse.dash();morse.c_space();
-      case 'y':morse.dash();morse.dot();morse.dash();morse.dash();morse.c_space();
-      case 'z':morse.dash();morse.dash();morse.dot();morse.dot();morse.c_space();
-      case 32:morse.w_space();
-
+      case 'a':morse.dot();morse.dash();morse.c_space();break;
+      case 'b':morse.dash();morse.dot();morse.dot();morse.c_space();break;
+      case 'c':morse.dash();morse.dot();morse.dot();morse.dot();morse.c_space();break;
+      case 'd':morse.dash();morse.dot();morse.dot();morse.c_space();break;
+      case 'e':morse.dot();morse.c_space();break;
+      case 'f':morse.dot(),morse.dot(),morse.dash(),morse.dot(),morse.c_space();break;
+      case 'g':morse.dash();morse.dash();morse.dot();morse.c_space();break;
+      case 'h':morse.dot();morse.dot();morse.dot();morse.dot();morse.c_space();break;
+      case 'i':morse.dot();morse.dot();morse.c_space();break;
+      case 'j':morse.dot();morse.dash();morse.dash();morse.dash();morse.c_space();break;
+      case 'k':morse.dash();morse.dot();morse.dash();morse.c_space();break;
+      case 'l':morse.dot();morse.dash();morse.dot();morse.dot();morse.c_space();break;
+      case 'm':morse.dash();morse.dash();morse.c_space();break;
+      case 'n':morse.dash();morse.dash();morse.c_space();break;
+      case 'o':morse.dash();morse.dash();morse.dash();morse.c_space();break;
+      case 'p':morse.dot();morse.dash();morse.dash();morse.dot();morse.c_space();break;
+      case 'q':morse.dash();morse.dash();morse.dot();morse.dash();morse.c_space();break;
+      case 'r':morse.dot();morse.dash();morse.dot();morse.c_space();break;
+      case 's':morse.dot();morse.dot();morse.dot();morse.c_space();break;
+      case 't':morse.dash();morse.c_space();break;
+      case 'u':morse.dot();morse.dot();morse.dash();morse.c_space();break;
+      case 'v':morse.dot();morse.dot();morse.dot();morse.dash();morse.c_space();break;
+      case 'w':morse.dot();morse.dash();morse.dash();morse.c_space();break;
+      case 'x':morse.dash();morse.dot();morse.dot();morse.dash();morse.c_space();break;
+      case 'y':morse.dash();morse.dot();morse.dash();morse.dash();morse.c_space();break;
+      case 'z':morse.dash();morse.dash();morse.dot();morse.dot();morse.c_space();break;
+      case 32:morse.w_space();break;
     }
   }
 
